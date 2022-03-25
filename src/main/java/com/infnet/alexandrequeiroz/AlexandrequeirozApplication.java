4fa7a99d1f5fd173a6ebfdcb88c9140b5e3e9168
apontes77 <alexandrepontesgyn@gmail.com>
2022-03-25 01:21:18 -0300

    first commit

---
+++ src/main/java/com/infnet/alexandrequeiroz/AlexandrequeirozApplication.java
@@ -0,0 +1,13 @@
+package com.infnet.alexandrequeiroz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlexandrequeirozApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlexandrequeirozApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/infnet/alexandrequeiroz/AlexandrequeirozApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infnet.alexandrequeiroz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlexandrequeirozApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
